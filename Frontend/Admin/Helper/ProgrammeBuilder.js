f27a589deb3dcfc9cd3dd2e1b6ec3a49bdec9790
gregoryvanko <gregory.vankoninckxloo@gmail.com>
2022-04-22 23:29:23 +0200

    New tenue seminaire

--- Frontend/Admin/Helper/ProgrammeBuilder.js
+++ Frontend/Admin/Helper/ProgrammeBuilder.js
@@ -1,13 +1,13 @@
 class ProgrammeBuilder {
     constructor(){}
 
-    static ViewNewProgrammeTenue({InDate = "", InConge = false, InTemple = "", InRite = "", InRepas = "" }={}){
-        let conteneur = NanoXBuild.DivFlexColumn(null, null, "width: 100%; align-content: start;")
+    static ViewNewProgrammeTenue({InDate="", InConge=false, InTemple="", InRite="", InRepas="", InSeminaireType="", InSeminaireLocal=""}={}){
+        let conteneur = NanoXBuild.DivFlexColumn()
         // Date de la tenue
-        let DivDate = NanoXBuild.DivFlexRowStart(null, null, "width: 100%; margin-bottom: 1rem;") 
+        let DivDate = NanoXBuild.DivFlexRowSpaceAround(null, null, "margin-bottom: 1rem;") 
         conteneur.appendChild(DivDate)
-        DivDate.appendChild(NanoXBuild.DivText("Date de la Tenue:", null, "Text InputLabelWidth", ""))
-        let InputDate = NanoXBuild.Input(InDate, "text", "InputDate", "", "InputDate", "Input Text", "width: 10rem;")
+        DivDate.appendChild(NanoXBuild.DivText("Date Tenue:", null, "Text InputLabelWidth", ""))
+        let InputDate = NanoXBuild.Input(InDate, "text", "InputDate", "", "InputDate", "Input Text", "width: 12rem;")
         InputDate.setAttribute("inputmode","none")
         DivDate.appendChild(InputDate)
         // https://mymth.github.io/vanillajs-datepicker
@@ -20,139 +20,73 @@ class ProgrammeBuilder {
             updateOnBlur : false
         });
         // Congé
-        let DivConge = NanoXBuild.DivFlexRowStart(null, null, "width: 100%; margin-bottom: 1rem;") 
+        let DivConge = NanoXBuild.DivFlexRowSpaceAround(null, null, "margin-bottom: 1rem;") 
         conteneur.appendChild(DivConge)
         DivConge.appendChild(NanoXBuild.DivText("En congé:", null, "Text InputLabelWidth", ""))
-        DivConge.appendChild(NanoXBuild.ToggleSwitch({Id : "Conge", Checked : InConge, OnChange : null, HeightRem : 2}))
+        let DivToogle = NanoXBuild.DivFlexRowEnd(null, null, "width: 12rem;")
+        DivConge.appendChild(DivToogle)
+        DivToogle.appendChild(NanoXBuild.ToggleSwitch({Id : "Conge", Checked : InConge, OnChange : null, HeightRem : 2}))
         // Temple
         const ListeOfTemple = ["Osiris", "Beauté"]
-        let DivTemple = NanoXBuild.DivFlexRowStart(null, null, "width: 100%; margin-bottom: 1rem;") 
-        conteneur.appendChild(DivTemple)
-        DivTemple.appendChild(NanoXBuild.DivText("Temple:", null, "Text InputLabelWidth", ""))
-        let inputtemple = NanoXBuild.Input(InTemple, "text", "InputTemple", "", "InputTemple", "Input Text", "width: 10rem")
-        inputtemple.autocomplete = "off"
-        inputtemple.setAttribute("inputmode","none")
-        DivTemple.appendChild(inputtemple)
-        autocomplete({
-            input: inputtemple,
-            minLength: 0,
-            showOnFocus: true,
-            //debounceWaitMs: 200,
-            emptyMsg: 'No suggestion',
-            fetch: function(text, update) {
-                text = text.toLowerCase();
-                var GroupFiltred = ListeOfTemple.filter(n => n.toLowerCase().startsWith(text))
-                var suggestions = []
-                GroupFiltred.forEach(element => {
-                    var MyObject = new Object()
-                    MyObject.label = element
-                    suggestions.push(MyObject)
-                });
-                update(suggestions);
-            },
-            onSelect: function(item) {
-                document.getElementById("InputTemple").value = item.label;
-            },
-            customize: function(input, inputRect, container, maxHeight) {
-                if (container.childNodes.length == 1){
-                    if (container.childNodes[0].innerText == 'No suggestion'){
-                        input.style.backgroundColor = "lightcoral"
-                    } else {
-                        input.style.backgroundColor = "white"
-                    }
-                } else {
-                    input.style.backgroundColor = "white"
-                }
-            },
-            disableAutoSelect: false
-        });
+        conteneur.appendChild(ProgrammeBuilder.BuildInput("Temple:", "InputTemple", InTemple, ListeOfTemple))
         // Rite
         const ListeOfRite = ["Mod", "REAA"]
-        let DivRite = NanoXBuild.DivFlexRowStart(null, null, "width: 100%; margin-bottom: 1rem;") 
-        conteneur.appendChild(DivRite)
-        DivRite.appendChild(NanoXBuild.DivText("Rite:", null, "Text InputLabelWidth", ""))
-        let inputrite = NanoXBuild.Input(InRite, "text", "InputRite", "", "InputRite", "Input Text", "width: 10rem")
-        inputrite.autocomplete = "off"
-        inputrite.setAttribute("inputmode","none")
-        DivRite.appendChild(inputrite)
-        autocomplete({
-            input: inputrite,
-            minLength: 0,
-            showOnFocus: true,
-            //debounceWaitMs: 200,
-            emptyMsg: 'No suggestion',
-            fetch: function(text, update) {
-                text = text.toLowerCase();
-                var GroupFiltred = ListeOfRite.filter(n => n.toLowerCase().startsWith(text))
-                var suggestions = []
-                GroupFiltred.forEach(element => {
-                    var MyObject = new Object()
-                    MyObject.label = element
-                    suggestions.push(MyObject)
-                });
-                update(suggestions);
-            },
-            onSelect: function(item) {
-                document.getElementById("InputRite").value = item.label;
-            },
-            customize: function(input, inputRect, container, maxHeight) {
-                if (container.childNodes.length == 1){
-                    if (container.childNodes[0].innerText == 'No suggestion'){
-                        input.style.backgroundColor = "lightcoral"
-                    } else {
-                        input.style.backgroundColor = "white"
-                    }
-                } else {
-                    input.style.backgroundColor = "white"
-                }
-            },
-            disableAutoSelect: false
-        });
+        conteneur.appendChild(ProgrammeBuilder.BuildInput("Rite:", "InputRite", InRite, ListeOfRite))
         // Repas
         const ListeOfRepas = ["Repas Frat", "Agapes", "Banquet", "Banquet rituel"]
-        let DivRepas = NanoXBuild.DivFlexRowStart(null, null, "width: 100%; margin-bottom: 1rem;") 
-        conteneur.appendChild(DivRepas)
-        DivRepas.appendChild(NanoXBuild.DivText("Repas:", null, "Text InputLabelWidth", ""))
-        let inputrepas = NanoXBuild.Input(InRepas, "text", "InputRepas", "", "InputRepas", "Input Text", "width: 10rem")
-        inputrepas.autocomplete = "off"
-        inputrepas.setAttribute("inputmode","none")
-        DivRepas.appendChild(inputrepas)
-        autocomplete({
-            input: inputrepas,
-            minLength: 0,
-            showOnFocus: true,
-            //debounceWaitMs: 200,
-            emptyMsg: 'No suggestion',
-            fetch: function(text, update) {
-                text = text.toLowerCase();
-                var GroupFiltred = ListeOfRepas.filter(n => n.toLowerCase().startsWith(text))
-                var suggestions = []
-                GroupFiltred.forEach(element => {
-                    var MyObject = new Object()
-                    MyObject.label = element
-                    suggestions.push(MyObject)
-                });
-                update(suggestions);
-            },
-            onSelect: function(item) {
-                document.getElementById("InputRepas").value = item.label;
-            },
-            customize: function(input, inputRect, container, maxHeight) {
-                if (container.childNodes.length == 1){
-                    if (container.childNodes[0].innerText == 'No suggestion'){
-                        input.style.backgroundColor = "lightcoral"
-                    } else {
-                        input.style.backgroundColor = "white"
-                    }
-                } else {
-                    input.style.backgroundColor = "white"
-                }
-            },
-            disableAutoSelect: false
-        });
+        conteneur.appendChild(ProgrammeBuilder.BuildInput("Repas:", "InputRepas", InRepas, ListeOfRepas))
+        // Seminaire type
+        const ListeOfTypeSeminaire = ["Pas de séminaire", "Travail", "Instruction"]
+        conteneur.appendChild(ProgrammeBuilder.BuildInput("Type séminaire:", "inputSeminaireType", InSeminaireType, ListeOfTypeSeminaire))
+        // Seminaire local
+        const ListeOfLocalSeminaire = ["Lumière"]
+        conteneur.appendChild(ProgrammeBuilder.BuildInput("Local séminaire:", "inputSeminaireLocal", InSeminaireLocal, ListeOfLocalSeminaire))
         // Ordre du jour
         // ToDo
         return conteneur
+    }
+
+    static BuildInput(Titre = "Titre", InputID= "InputId", InitValue = "", ListeOfValue = []){
+        let Div = NanoXBuild.DivFlexRowSpaceAround(null, null, "margin-bottom: 1rem;") 
+        Div.appendChild(NanoXBuild.DivText(Titre, null, "Text InputLabelWidth", ""))
+        let Myinput = NanoXBuild.Input(InitValue, "text", InputID, "", InputID, "Input Text", "width: 12rem")
+        Myinput.autocomplete = "off"
+        Myinput.setAttribute("inputmode","none")
+        Div.appendChild(Myinput)
+        autocomplete({
+            input: Myinput,
+            minLength: 0,
+            showOnFocus: true,
+            //debounceWaitMs: 200,
+            emptyMsg: 'No suggestion',
+            fetch: function(text, update) {
+                text = text.toLowerCase();
+                var GroupFiltred = ListeOfValue.filter(n => n.toLowerCase().startsWith(text))
+                var suggestions = []
+                GroupFiltred.forEach(element => {
+                    var MyObject = new Object()
+                    MyObject.label = element
+                    suggestions.push(MyObject)
+                });
+                update(suggestions);
+            },
+            onSelect: function(item) {
+                document.getElementById(InputID).value = item.label;
+            },
+            customize: function(input, inputRect, container, maxHeight) {
+                if (container.childNodes.length == 1){
+                    if (container.childNodes[0].innerText == 'No suggestion'){
+                        input.style.backgroundColor = "lightcoral"
+                    } else {
+                        input.style.backgroundColor = "white"
+                    }
+                } else {
+                    input.style.backgroundColor = "white"
+                }
+            },
+            disableAutoSelect: false
+        });
+        return Div
     }
 
     static ViewNewProgrammeCod(){
